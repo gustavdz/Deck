94c8f56d9a47117e75f807d1d40f3594becd99bc
Gustavo Decker <gustavdz@gmail.com>
2017-07-16 20:21:50 -0500

    sandbox funcionando con paypal

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -18,7 +18,7 @@ export class CheckoutPage {
   billing_shipping_same: boolean;
   userInfo: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public alertCtrl: AlertController, public paypal: PayPal) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public alertCtrl: AlertController, public payPal: PayPal) {
     this.newOrder = {};
     this.newOrder.billing_address = {};
     this.newOrder.shipping_address = {};
@@ -91,7 +91,69 @@ export class CheckoutPage {
 
 
     if(paymentData.method_id == "paypal"){
-      //todo
+
+      this.payPal.init({
+        PayPalEnvironmentProduction: "YOUR_PRODUCTION_CLIENT_ID",
+        PayPalEnvironmentSandbox: "AaifUGJYfZDOTyCW7p64Z8z6cvbTfzUQptI7Ef7IYWhwESB0e7nVi1BUZiaovnjWaRmPAiy-okq9j6_E"
+      }).then(() => {
+        // Environments: PayPalEnvironmentNoNetwork, PayPalEnvironmentSandbox, PayPalEnvironmentProduction
+        this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({
+          // Only needed if you get an "Internal Service Error" after PayPal login!
+          //payPalShippingAddressOption: 2 // PayPalShippingAddressOptionPayPal
+        })).then(() => {
+
+          this.storage.get("cart").then((cart) => {
+
+            let total = 0.00;
+            cart.forEach((element, index) => {
+              orderItems.push({ product_id: element.product.id, quantity: element.qty });
+              total = total + (element.product.price * element.qty);
+            });
+
+            let payment = new PayPalPayment(total.toString(), 'USD', 'Description', 'sale');
+            this.payPal.renderSinglePaymentUI(payment).then((response) => {
+              // Successfully paid
+
+              alert(JSON.stringify(response));
+
+
+              data.line_items = orderItems;
+              //console.log(data);
+              let orderData: any = {};
+
+              orderData.order = data;
+
+              this.WooCommerce.postAsync('orders', orderData).then((data) => {
+                alert("Order placed successfully!");
+
+                let response = (JSON.parse(data.body).order);
+
+                this.alertCtrl.create({
+                  title: "Order Placed Successfully",
+                  message: "Your order has been placed successfully. Your order number is " + response.order_number,
+                  buttons: [{
+                    text: "OK",
+                    handler: () => {
+                      this.navCtrl.setRoot(HomePage);
+                    }
+                  }]
+                }).present();
+              })
+
+            })
+
+          }, () => {
+            // Error or render dialog closed without being successful
+          });
+        }, () => {
+          // Error in configuration
+        });
+      }, () => {
+        // Error in initialization, maybe PayPal isn't supported or something else
+      });
+
+
+
 
     } else {
 
